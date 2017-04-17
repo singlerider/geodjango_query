a28d9693cb2fad8a566c16f79c1a7ab03482c2ab
shane <haroboy876@gmail.com>
2017-04-17 10:33:14 -0700

    define initial model

--- ridecell/parking_app/models.py
+++ ridecell/parking_app/models.py
@@ -3,4 +3,44 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class ParkingSpot(models.Model):
+    latitude = models.FloatField(  # I'd implement DecimalField with more time
+        default=0.0
+    )
+    longitude = models.FloatField(  # I'd implement DecimalField with more time
+        default=0.0
+    )
+    # spot_number is the local number associated with a parking spot, not an id
+    spot_number = models.PositiveIntegerField()  # no need for negative numbers
+
+    def find_spots_in_radius(self, radius=None):  # radius in meters
+        pass
+        # TODO: if type(radius) == int, return mocked results.
+        #   if type(radius) != int, return all
+
+
+class ParkingSpotReservation(models.Model):
+    parking_spot = models.ForeignKey(
+        'ParkingSpot',
+        models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    user = models.ForeignKey(
+        'User',
+        models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    reservation_begin = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=False, null=False
+    )
+    reservation_end = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=False, null=False
+    )
